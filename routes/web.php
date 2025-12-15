03e2a0dbf0650a54652af93dc767b950612de4f9
MRX00777 <mrx31995@gmail.com>
2025-12-15 12:51:12 +0500

    Однометодные контроллеры

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,18 @@
 <?php
 
-use App\Http\Controllers\MainController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
-
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\MainController;
+use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\Post\IndexController;
+use App\Http\Controllers\Post\CreateController;
+use App\Http\Controllers\Post\StoreController;
+use App\Http\Controllers\Post\ShowController;
+use App\Http\Controllers\Post\EditController;
+use App\Http\Controllers\Post\UpdateController;
+use App\Http\Controllers\Post\DestroyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +25,22 @@ use App\Http\Controllers\ContactController;
 |
 */
 
+
+
+    Route::get('/posts', IndexController::class)->name('post.index');
+
+    Route::get('/posts/create', CreateController::class)->name('post.create');
+
+    Route::post('/posts', StoreController::class)->name('post.store');
+
+    Route::get('/posts/{post}', ShowController::class)->name('post.show');
+
+    Route::patch('/posts/{post}', UpdateController::class)->name('post.update');
+    Route::delete('/posts/{post}', DestroyController::class)->name('post.delete');
+
+    Route::get('/posts/{post}/edit', EditController::class)->name('post.edit');
+
+
 Route::get('/', [MainController::class, 'index'])->name('main.index');
 
 
@@ -29,16 +51,5 @@ Route::get('/contact', [ContactController::class, 'index'])->name('contact.index
 
 
 
-Route::get('/posts', [PostController::class, 'index'])->name('post.index');
 
-Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');
-
-Route::post('/posts', [PostController::class, 'store'])->name('post.store');
-
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('post.show');
-
-Route::patch('/posts/{post}', [PostController::class, 'update'])->name('post.update');
-Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('post.delete');
-
-Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
 
